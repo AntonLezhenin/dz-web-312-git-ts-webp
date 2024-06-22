8099eaa946ce7456ea1b8c020e11338db7a82d91
AntonLezhenin <anton4615@gmail.com>
2024-06-22 15:18:24 +0300

    dobavka3zd

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-const plusButton : HTMLElement | null = document.querySelector(".plus");
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  const txt : HTMLElement | null = document.querySelector(".txt");
 
-let counter : HTMLElement | null = document.querySelector(".counter");
+  const palitra : HTMLElement | null = document.querySelector(".palitra");
 
-const minusButton : HTMLElement | null = document.querySelector(".minus");
+  const children = palitra.getElementsByTagName("div");
 
-
-let start : number = 0;
-
-plusButton.addEventListener("click", function(){
-    start++;
-    counter.innerText = `${start}`;
-    return start
-});
-
-minusButton.addEventListener("click", function(){
-    start--;
-    counter.innerText = `${start}`;
-    return start
-})
+  for(let i = 0; i <= 10; i++){
+   let block : HTMLElement = document.createElement("div");
+   block.style.width = "50px";
+   block.style.height = "50px";
+   block.style.backgroundColor = getRandomColor();
+   block.dataset.color = block.style.backgroundColor;
+   palitra.appendChild(block)
+   block.addEventListener("click", function(){
+   txt.style.color = block.dataset.color;
+   })
+  }
