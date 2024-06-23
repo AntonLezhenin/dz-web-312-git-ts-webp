fc40ce0780a41be65c2c7230aab38d8ed0aafd28
AntonLezhenin <anton4615@gmail.com>
2024-06-23 14:26:23 +0300

    did4zadanie

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,40 @@
-const plusButton : HTMLElement | null = document.querySelector(".plus");
+const textForCom : HTMLElement | null = document.querySelector(".textForCom");
 
-let counter : HTMLElement | null = document.querySelector(".counter");
+const addComBut : HTMLElement | null = document.querySelector(".addComBut");
 
-const minusButton : HTMLElement | null = document.querySelector(".minus");
+const nameCom : HTMLElement | any = document.querySelector(".name");
+
+const comment : HTMLElement | any = document.querySelector(".comment");
 
 
-let start : number = 0;
+const comments : HTMLElement | null = document.querySelector(".comments");
 
-plusButton.addEventListener("click", function(){
-    start++;
-    counter.innerText = `${start}`;
-    return start
+const comArr = [];
+
+let glName : string;
+
+nameCom.addEventListener("input", function(e : any){
+    glName = e.target.value;
+})
+
+
+let glCom : any;
+comment.addEventListener("input", function(e : any){
+  glCom = e.target.value;
 });
 
-minusButton.addEventListener("click", function(){
-    start--;
-    counter.innerText = `${start}`;
-    return start
+addComBut.addEventListener("click", function(){
+    let comBlock : HTMLElement | null = document.createElement("div");
+    let nomi : HTMLElement | null = document.createElement("h3");
+    nomi.innerText = glName;
+    comBlock.appendChild(nomi);
+    let date : HTMLElement | null = document.createElement("p");
+    date.innerText = `${new Date()}`;
+    comBlock.appendChild(date);
+    let comi : HTMLElement | null = document.createElement("p");
+    comi.innerText = glCom;
+    comBlock.appendChild(comi);
+    comments.appendChild(comBlock);
+    nameCom.value = "";
+    comment.value = "";
 })
